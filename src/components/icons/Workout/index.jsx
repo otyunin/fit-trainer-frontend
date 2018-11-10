b0cdf746a4354a8b1e304afcba47bc59f91a5a1f
Oleg Tyunin <otyunin06@gmail.com>
2018-11-10 20:54:09 +0200

    Custom icons

---
+++ src/components/icons/Workout/index.jsx
@@ -0,0 +1,13 @@
+/* eslint-disable max-len */
+import React from 'react'
+import SvgIcon from '@material-ui/core/SvgIcon'
+
+function WorkoutIcon(props) {
+  return (
+    <SvgIcon {...props} viewBox="0 0 512 512">
+      <path d="M120,437h271.9V105H120V437z M225.1,156.4h121.8v30H225.1V156.4z M225.1,222.8h121.8v30H225.1V222.8z M225.1,289.2h121.8v30H225.1V289.2z M225.1,355.6h121.8v30H225.1V355.6z M165,156.4h30v30h-30V156.4z M165,222.8h30v30h-30V222.8z M165,289.2h30v30h-30V289.2z M165,355.6h30v30h-30V355.6z M361.9,30v45h60v392H90V75h60V30H45v482h421.9V30H361.9z M180,75h152 V26.5c0-14.6-11.9-26.5-26.5-26.5h-99C191.8,0,180,11.9,180,26.5V75z" />
+    </SvgIcon>
+  )
+}
+
+export default WorkoutIcon
---
+++ src/components/icons/index.js
@@ -0,0 +1,5 @@
+export { default as CalendarIcon } from 'components/icons/Calendar'
+export { default as WorkoutIcon } from 'components/icons/Workout'
+export { default as WorkoutEditIcon } from 'components/icons/WorkoutEdit'
+export { default as ExerciseIcon } from 'components/icons/Exercise'
+export { default as ExerciseEditIcon } from 'components/icons/ExerciseEdit'
